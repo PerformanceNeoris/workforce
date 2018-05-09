7c5cdea08c49ce4c1572e27af8827a653252ec66
PerformanceNeoris <performanceneoris@gmail.com>
2018-05-09 11:21:46 -0300

    Nueva versión de Screenshot que permite grabar las capturas recibiendo los parámetros: driver: Nombre del Driver utilizado (Chrome, Firefox, etc) caso: Nombre del caso de prueba nombre: Nombre de la imagen a capturar

--- libreriaUtiles.py
+++ libreriaUtiles.py
@@ -4,10 +4,27 @@ __email__ = "martin.delpercio@neoris.com"
 __name__ = "Libreía de Funciones"
 
 import datetime
+import os
 
-def screenShot(driver,nombre):
+def screenShot(driver,caso,nombre):
+
     # SCREEN SHOT
+
+    # Arma la Fecha y Hora
     fechahora = datetime.datetime.now().strftime("%d%m%y%H%M%S")
-    locacion = "C:\\Capturas\\"
-    captura = locacion + nombre + fechahora + ".png"
+
+    # Verifica si existe la carpeta de la Fecha, sino la crea
+    file_path = "C:\\Capturas\\" + fechahora
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Verifica si existe la carpeta del caso, sino la crea
+    file_path = "C:\\Capturas\\" + fechahora + nombre
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Define la carpeta de Capturas
+    captura = "C:\\Capturas\\" + fechahora + "\\" + nombre + ".png"
     driver.get_screenshot_as_file(captura)
