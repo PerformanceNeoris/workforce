d7d3cf7ea622ebcd0d6c3cb80eed74b3ba898783
PerformanceNeoris <performanceneoris@gmail.com>
2018-05-09 13:54:23 -0300

    Nueva versión de Screenshots que permite grabar las capturas recibiendo parámetros. Contiene dos métodos, uno para capturas en la Web y otra para en las aplicaciones de Windows

--- libreriaUtiles.py
+++ libreriaUtiles.py
@@ -1,14 +1,15 @@
 __author__ = "mjdp"
 __version__ = "1.0.0"
 __email__ = "martin.delpercio@neoris.com"
-__name__ = "Libreía de Funciones"
+__name__ = "Librería de Funciones"
 
 import datetime
 import os
+import pyautogui
 
-def screenShot(driver,caso,nombre):
+def screenShotWeb(driver,caso,nombre):
 
-    # SCREEN SHOT
+    # SCREEN SHOT para Web Page
 
     # Arma la Fecha y Hora
     fechahora = datetime.datetime.now().strftime("%d%m%y%H%M%S")
@@ -28,3 +29,35 @@ def screenShot(driver,caso,nombre):
     # Define la carpeta de Capturas
     captura = "C:\\Capturas\\" + fechahora + "\\" + nombre + ".png"
     driver.get_screenshot_as_file(captura)
+
+def screenShotGUI(caso,nombre):
+
+    # SCREEN SHOT
+
+    localFolder = "C:\\Evidencia"
+
+    # Verifica si existe la carpeta Evidencia, sino la crea
+    file_path = localFolder
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Arma las variables
+    fechahora = datetime.datetime.now().strftime("%d%m%y%H%M%S")
+    fecha = datetime.datetime.now().strftime("%d%m%y")
+    hora = datetime.datetime.now().strftime("%H%M%S")
+
+    # Verifica si existe la carpeta de la Fecha, sino la crea
+    file_path = localFolder + "\\" + fecha
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+    # Verifica si existe la carpeta del caso, sino la crea
+    file_path = localFolder + "\\" + fecha + "\\" + caso
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+    # Define la carpeta de Capturas
+    nombre = nombre + hora
+    captura = localFolder + "\\" + fecha + "\\" + caso + "\\" + nombre + ".png"
+    pyautogui.screenshot(captura)
