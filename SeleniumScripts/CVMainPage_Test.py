9ba6ef89c5f09236b0d988221f1486ae94c2f62b
PerformanceNeoris <performanceneoris@gmail.com>
2018-05-11 17:26:25 -0300

    Nueva versión sin usar archivos o variables globales

--- SeleniumScripts/CVMainPage_Test.py
+++ SeleniumScripts/CVMainPage_Test.py
@@ -3,6 +3,7 @@ __version__ = "1.0.0"
 __email__ = "mdelpercio@teco.com.ar"
 
 import time
+import pyautogui
 from SeleniumScripts.CVLoginPage import CVLoginPage
 from SeleniumScripts.CVMainPage import CVMainPage
 from selenium import webdriver
@@ -11,7 +12,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 #DEFINICIONES
 global URL
 URL="http://www.cablevisionfibertel.com.ar"
-#URL="https://auth.cablevision.com.ar/saml/authenticationendpoint/login.do?sessionDataKey=1ed31b8f-db5d-465c-bdc3-3b331ccb7f15&relyingParty=CablevisionDigital&type=samlsso&sp=CablevisionDigital&isSaaSApp=false&authenticators=BasicAuthenticator:LOCAL#"
 driver = webdriver.Chrome('C:\\Webdrivers\\chromedriver.exe')
 #driver.set_page_load_timeout(10)
 driver.get(URL)
@@ -20,28 +20,15 @@ time.sleep(5)
 driver.maximize_window()
 wait = WebDriverWait(driver, 5)
 
-usuario = "federicomarilungo@gmail.com"
-clave = "cable12"
-
-#Crea el archivo
-file = open("URL.txt","w")
-file.close()
+usuario = "martinjavierd@gmail.com"
+clave = "CrudSec2"
 
 CVMain = CVMainPage(driver)
 CVMain.clickIngresar()
 
-driver.quit()
-
-file = open("URL.txt", "r")
-textURL = file.read()
-print(textURL)
-
-driver = webdriver.Chrome('C:\\Webdrivers\\chromedriver.exe')
-driver.get(textURL)
-time.sleep(5)
-file.close()
-driver.maximize_window()
-wait = WebDriverWait(driver, 5)
+pyautogui.moveTo(5, 505)
+time.sleep(1)
+pyautogui.click(5, 505,3)
 
 CVLogin = CVLoginPage(driver)
 CVLogin.ingresar(usuario, clave)
